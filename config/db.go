8860cfab670227fa75c96e7ae11c2eea92bcd88b
Jaysdes <akungameming01@gmail.com>
2025-08-20 14:11:27 +0700

    Add new feature to process user data

--- config/db.go
+++ config/db.go
@@ -18,9 +18,8 @@ func ConnectDB() {
 	port := os.Getenv("MYSQLPORT")
 	dbname := os.Getenv("MYSQLDATABASE")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		user, pass, host, port, dbname,
-	)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local&timeout=5s",
+		user, pass, host, port, dbname)
 
 	database, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
